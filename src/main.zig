0615a2f094e54016b4ef122502cf0274478eb324
Biraj <biraj.pub@gmail.com>
2024-08-31 13:59:15 +0530

    feat: print command not found msg

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,9 @@ pub fn main() !void {
     var buffer: [1024]u8 = undefined;
     const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    // TODO: Handle user input
-    _ = user_input;
+    var it = std.mem.split(u8, user_input, " ");
+    const command = it.next() orelse "";
+    if (command.len > 0) {
+        try stdout.print("{s}: command not found\n", .{command});
+    }
 }
