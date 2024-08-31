bbc7bb12acfea8aef8a634ce08d89a0fd906d373
Biraj <biraj.pub@gmail.com>
2024-08-31 16:03:33 +0530

    fix: function for each builtin

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,16 @@
 const std = @import("std");
 
+const FnType = *const fn (args_it: *std.mem.SplitIterator(u8, .sequence)) anyerror!void;
+
+var builtinsHash: std.StringHashMap(FnType) = undefined;
+
 pub fn main() !void {
+    builtinsHash = std.StringHashMap(FnType).init(std.heap.page_allocator);
+    defer builtinsHash.deinit();
+
+    try builtinsHash.put("echo", &echo);
+    try builtinsHash.put("exit", &exit);
+
     while (true) {
         const stdout = std.io.getStdOut().writer();
         try stdout.print("$ ", .{});
@@ -10,31 +20,42 @@ pub fn main() !void {
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
         var it = std.mem.split(u8, user_input, " ");
+
         const command = it.next() orelse "";
         if (command.len == 0) {
             continue;
         }
 
-        if (std.mem.eql(u8, command, "exit")) {
-            const status_arg = it.next() orelse "";
-            const status = std.fmt.parseInt(u8, status_arg, 10) catch @as(u8, 0);
-            std.process.exit(status);
-        } else if (std.mem.eql(u8, command, "echo")) {
-            var is_first_arg = true;
-            while (it.next()) |arg| {
-                if (!is_first_arg) {
-                    try stdout.print(" ", .{});
-                } else {
-                    is_first_arg = false;
-                }
-
-                try stdout.print("{s}", .{arg});
-            }
-
-            try stdout.print("\n", .{});
+        const builtin = builtinsHash.get(command);
+        if (builtin) |f| {
+            try f(&it);
             continue;
         }
 
         try stdout.print("{s}: command not found\n", .{command});
     }
+}
+
+fn echo(args_it: *std.mem.SplitIterator(u8, .sequence)) anyerror!void {
+    const stdout = std.io.getStdOut().writer();
+
+    var is_first_arg = true;
+    while (args_it.next()) |arg| {
+        if (!is_first_arg) {
+            try stdout.print(" ", .{});
+        } else {
+            is_first_arg = false;
+        }
+
+        try stdout.print("{s}", .{arg});
+    }
+
+    try stdout.print("\n", .{});
+}
+
+fn exit(args_it: *std.mem.SplitIterator(u8, .sequence)) anyerror!void {
+    var status: u8 = 0;
+    const status_arg = args_it.next() orelse "";
+    status = std.fmt.parseInt(u8, status_arg, 10) catch @as(u8, 0);
+    std.process.exit(status);
 }
