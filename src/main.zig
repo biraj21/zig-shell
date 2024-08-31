65c474e0bb2eba498997d47c8cce8e45d587747f
Biraj <biraj.pub@gmail.com>
2024-08-31 14:41:11 +0530

    feat: handle exit builtin

--- src/main.zig
+++ src/main.zig
@@ -11,8 +11,16 @@ pub fn main() !void {
 
         var it = std.mem.split(u8, user_input, " ");
         const command = it.next() orelse "";
-        if (command.len > 0) {
-            try stdout.print("{s}: command not found\n", .{command});
+        if (command.len == 0) {
+            continue;
         }
+
+        if (std.mem.eql(u8, command, "exit")) {
+            const status_arg = it.next() orelse "";
+            const status = std.fmt.parseInt(u8, status_arg, 10) catch @as(u8, 0);
+            std.os.exit(status);
+        }
+
+        try stdout.print("{s}: command not found\n", .{command});
     }
 }
