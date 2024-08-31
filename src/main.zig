dd2afe003c3cf7b9e68656fd7cd18fc3d2edea96
Biraj <biraj.pub@gmail.com>
2024-08-31 14:01:19 +0530

    feat: init REPL

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,18 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
+    while (true) {
+        const stdout = std.io.getStdOut().writer();
+        try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    var it = std.mem.split(u8, user_input, " ");
-    const command = it.next() orelse "";
-    if (command.len > 0) {
-        try stdout.print("{s}: command not found\n", .{command});
+        var it = std.mem.split(u8, user_input, " ");
+        const command = it.next() orelse "";
+        if (command.len > 0) {
+            try stdout.print("{s}: command not found\n", .{command});
+        }
     }
 }
